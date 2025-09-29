3e839f970ec48bded7054165ee02a358c7f3e76a
Kamini-Prajapati <prajapatikamini69@gmail.com>
2025-09-29 23:20:01 +0530

    Add files via upload

--- 1_to 30_programs.py
+++ 1_to 30_programs.py
@@ -11,7 +11,80 @@
 # tuple_len = len(tuple)
 # print("length of tuple = ",tuple_len)
 
-my_tuple = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-slice_obj = slice(2, 7, 2) # Defines a slice from index 2 to 7 (exclusive), with a step of 2
-sliced_result = my_tuple[slice_obj]
-print(f"Sliced tuple: {sliced_result}") # Output: (30, 50, 70)
+# Q4
+# tuple = (10, 20, 30, 40, 50)
+# print("Elements from index 1 to 3 = ", tuple[1:4])
+# print("Elements from the beginning to index 2 = ", tuple[ :3])
+# print("Elements from index 3 to the end = ", tuple[3: ])
+
+# Q5
+# 1
+# tuple = ("audi", "swift", "defender")
+# print("first element of tuple = ",tuple[0])
+# print("second element of tuple = ",tuple[1])
+# 2
+# tuple = ("audi", "swift", "defender")
+# indexing_tuple = int(input("enter a index to show an element of tuple = "))
+# print("Show Element of tuple = ",tuple[indexing_tuple])
+
+# Q6
+# Q7
+# Q8
+# Q9
+# Q10
+# n = 8
+# tuple = ('PYTHON , ')
+# repeated_tuple = tuple * n
+# print("Tuple repeated", n, "times: ", repeated_tuple)
+
+# Q11
+# tuple = (10, 20, 30)
+# tuple_list = list(tuple)
+# print("Original Tuple = ", tuple)
+# print("Tuple to list = ", tuple_list)
+
+# Q12
+# 1
+# tuple = (10, 20, 30, 40, 50)
+# tuple_string = str(tuple)
+# print("Original Tuple = ", tuple)
+# print("Tuple to string = ", tuple_string) 
+# 2
+# tuple = ("kamini", "kavita", "komal")
+# tuple_string = str(tuple)
+# print("Original Tuple = ", tuple)
+# print("Tuple to string = ", tuple_string)
+
+# Q13
+# tuple = (6, 80, 10, 4, 9)
+# max_value = max(tuple)
+# min_value = min(tuple)
+# print("Maximum value in the tuple: ,max_value)
+# print("Minimum value in the tuple: ,min_value)
+
+# Q14
+# tuple = (5, 1, 9, 3, 7)
+# sum_value = sum(tuple)
+# print(f"Maximum value in the tuple: ",sum_value)
+
+# Q15
+# tuple = (10, 20, 5, 40, 15)
+# sorted_tuple = sorted(tuple)
+# print("Sorted tuple: ", sorted_tuple)
+
+# Q16
+# tuple = ("rose", "lily", "jasmine", "tulip")
+# sorted_tuple = sorted(tuple)
+# print("Sorted tuple: ", sorted_tuple)
+
+# Q17
+# tuple = (10, 20, 30, 40, 50)
+# rev_tuple = tuple[::-1]
+# print("Reversed tuple: ", rev_tuple)
+
+# Q18
+tuple1 = ('red', 'green', 'blue', 'yellow')
+tuple2 = (1, 2, 3, 4)
+if tuple1 and tuple2:
+    concatenated_tuple = tuple1 + tuple2
+    print("Concatenated tuple: ", concatenated_tuple)
